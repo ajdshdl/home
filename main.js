dcf63e846f02f921277c8e2b78fd150b9c04ab9b
garuru <92495123+ajdshdl@users.noreply.github.com>
2023-02-22 15:02:18 +0900

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import store from '@/scripts/store'
-import router from '@/scripts/router'
-import App from './App.vue'
-
-
-
-createApp(App).use(store).use(router).mount('#app')
