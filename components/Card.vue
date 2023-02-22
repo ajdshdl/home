858eca26962e959ba6283a69174b67163f752999
garuru <92495123+ajdshdl@users.noreply.github.com>
2023-02-22 14:56:47 +0900

    Add files via upload

---
+++ components/Card.vue
@@ -0,0 +1,84 @@
+<!--
+<template>
+<div class="card shadow-sm">
+	<img :src="item.imgPath" />
+	<div class="card-body">
+		<p class="card-text">이름: {{item.name}}</p>
+		<div class="d-flex justify-content-between align-items-center">
+		<div class="btn-group">
+			<button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+			<button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+		</div>
+		<small class="text-muted">9 mins</small>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'Card',
+	props: {
+		item: Object
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
+-->
+
+<template>
+<div class="card shadow-sm">
+	<span class="img" :style="{backgroundImage: `url(${item.imgPath})`}" />
+	<div class="card-body">
+		<p class="card-text"><span>{{ item.name }}&nbsp;</span>
+			<span class="discount badge bg-danger">{{item.discountPer}}%</span>
+		</p>
+		<div class="d-flex justify-content-between align-items-center">
+
+			<button class="btn btn-primary" @click="addToCart(item.id)"><i class="fa fa-shopping-cart" aria-hidden="true"></i></button>
+
+			<small class="price text-muted"> 
+				{{lib.getNumberFormatted(item.price)}} 원
+			</small>
+			<small class="discount text-danger text-muted">
+				{{ lib.getNumberFormatted(item.price - (item.price * item.discountPer / 100)) }} 원
+			</small>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+import lib from "@/scripts/lib";
+import axios from 'axios';
+export default {
+	name: 'Card',
+	props: {
+		item: Object
+	},
+	setup(){
+		const addToCart = (itemId)=>{
+			axios.post(`/api/cart/items/${itemId}`).then(()=>{
+				console.log('success')
+			})
+		}
+		return {lib, addToCart}
+	}
+}
+</script>
+
+<style scoped>
+.card .img{
+	display: inline-block;
+	width: 100%;
+	height: 250px;
+	background-size: cover;
+	background-position: center;
+}
+.card .card-body .price{
+	text-decoration: line-through;
+}
+</style>
